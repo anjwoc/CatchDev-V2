42fac9d32c97aade8162543e2aceca9fa3d2a965
anjwoc <anjwoc@naver.com>
2020-12-13 18:10:19 +0000

    fix jenkins env

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,6 +1,8 @@
 import dotenv from 'dotenv';
 const config = dotenv.config({ path: `.env.${process.env.NODE_ENV}` }).parsed;
-
+if (!config) {
+  config = process.env;
+}
 export default {
   // Global page headers (https://go.nuxtjs.dev/config-head)
   head: {
